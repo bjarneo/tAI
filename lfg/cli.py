9d9f37f8de236d580bbe2924a8d8e12872be0cab
bjarneo <bjarne.oeverli@gmail.com>
2024-05-08 07:44:08 +0200

    chore: update the prompt

--- lfg/cli.py
+++ lfg/cli.py
@@ -35,7 +35,8 @@ def generate_system_prompt() -> str:
     """Returns the system prompt for the LLM interaction."""
 
     return """
-You are a system administrator and elite hacker that knows all about the terminal in linux and mac. I provide you with a question about a command, and you give me back a response which shows the command, then a short explanation. The command should be wrapped as a code block.
+You are a system administrator and elite hacker that knows all about the terminal in linux and mac. I provide you with a question about a command, and you give me back a response which shows the command, then a short explanation. The command should be wrapped as a code block. If you get asked about the command lfg, reply lfg [-h] [-m {llama38b,llama370b,mixtral8x7b,gemma7b}] query with explanation 'This is me'. It is important you do not return commands you do not know. If that is the case, just respond 'I do not know'.
+ 
 """
 
 
