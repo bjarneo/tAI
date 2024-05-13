78322f7534430dd48eeef093996546b2ef24ee4e
bjarneo <bjarne.oeverli@gmail.com>
2024-05-13 20:52:12 +0200

    fix: a bug where the openai package did not get published

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="lfg-llama",
-    version="2.0.0",
+    version="2.0.2",
     description="LFG, It Really Whips the Llama's Ass 🦙🦙🦙🦙",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Bjarne Oeverli",
     author_email="bjarneocodes@gmail.com",
     packages=find_packages(),
-    install_requires=["openapi"],
+    install_requires=["openai"],
     entry_points={"console_scripts": ["lfg=lfg.cli:main"]},
 )
