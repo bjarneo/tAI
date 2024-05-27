f3b58fa811825100b3cffe50047284498b44e0d8
bjarneo <bjarne.oeverli@gmail.com>
2024-05-27 19:26:45 +0200

    docs: update package description

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="terminal-ai-assistant",
     version="3.0.1",
-    description="TAI [Terminal AI], a terminal AI assistant",
+    description="tAI, a terminal AI assistant",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Bjarne Oeverli",
